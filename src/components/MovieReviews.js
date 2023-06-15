6089c382aeb63d75f2ee990e701b6234558f0c57
Daniel Suarez <suarez.daniel101@gmail.com>
2023-06-14 23:28:16 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
 // Code MovieReviews Here
+import React from "react";
+
+const MovieReviews = ({ reviews }) => {
+  // console.log(reviews)
+  return (<ul className="review-list">
+    {reviews.map((review) => (
+      <li className="review">Movie Title: {review.display_title}
+      <h4>Reviewer: {review.byline}</h4>
+      <p>HeadLine: {review.headline}</p>
+      <p>Summary: {review.summary_short}</p>
+      </li>
+    ))}
+  </ul>)
+};
+
+
+export default MovieReviews;
